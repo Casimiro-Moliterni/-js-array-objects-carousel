a3d4065f50383542d4ed915327d6d3893b409bca
Casimiro Moliterni <moliternicasimiro@hotmail.it>
2024-03-21 16:44:20 +0100

    add: js img

--- js/script.js
+++ js/script.js
@@ -15,26 +15,50 @@
 
 const images = [
     {
-        image: 'img/01.webp',
+        image: '01.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
     }, {
-        image: 'img/02.webp',
+        image: '02.webp',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
     }, {
-        image: 'img/03.webp',
+        image: '03.webp',
         title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
     }, {
-        image: 'img/04.webp',
+        image: '04.webp',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
     }, {
-        image: 'img/05.webp',
+        image: '05.webp',
         title: "Marvel's Avengers",
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
 
-let activeDiv = 0 ;
+let activeItem = 0 ;
+
+const imagesContainer = document.querySelector('.img-container');
+const imgRightContainer = document.querySelector('.img-right-container');
+
+
+images.forEach((card) => {
+
+    const newCardimg = `
+    <div class="image active">
+                <img src="img/${card.image}" alt="">
+                <div class="text-img">
+                   <h1 >${card.title}</h1>
+                   <p >${card.text}</p>
+    `;
+    
+    const newCardRightimg = `
+    <div class="card ">
+    <img src="img/${card.image}" alt="">
+    </div>
+    `;
+
+    imagesContainer.innerHTML += newCardimg;
+    imgRightContainer.innerHTML += newCardRightimg;
+});
